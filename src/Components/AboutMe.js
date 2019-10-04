81fd4b51e310c401b92f77cdb755fb2aa8d81e6e
Eric Jobin <ejobin@gmail.com>
2019-10-04 14:30:14 -0400

    Set up page divisions

---
+++ src/Components/AboutMe.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+const styles = {
+	backdrop: {
+		background: "red",
+		height: "100vh",
+		  
+	},
+	headingStyle: {
+	  	fontSize: 100
+	}
+};
+
+class AboutMe extends Component {
+
+	render(){
+
+		return(
+			<div style = {styles.backdrop}>
+
+			</div>
+		)
+	}
+}
+
+export default AboutMe;
---
+++ src/Pages/Home.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import AboutMe from "../Components/AboutMe";
+
+class Home extends Component {
+
+
+	render(){
+
+		return(
+			<div>
+
+				<AboutMe/>
+				{/* <Portfolio/> */}
+				{/* <Resume/> */}
+				{/* <Links/> */}
+
+			</div>
+		)
+	}
+}
+
+export default Home;
