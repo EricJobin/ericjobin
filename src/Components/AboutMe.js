fb1d0eec3b2e1ac6760dce93e03ad55f7d91a3bf
Eric Jobin <ejobin@gmail.com>
2019-10-22 14:02:25 -0400

    Work in progress

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
+import Face from "../Assets/ericJobin.jpg";
 
 const styles = {
 	backdrop: {
 		background: "red",
 		height: "100vh",
+		padding: "0",
+		margin: "0",
 		  
 	},
-	headingStyle: {
-	  	fontSize: 100
+	portrait: {
+		height: "40vh",
+		float: "left",
+		// width: "100%",
 	}
 };
 
@@ -17,6 +22,12 @@ class AboutMe extends Component {
 
 		return(
 			<div style = {styles.backdrop}>
+				<span>
+					<img src={Face} style={styles.portrait}/>
+				</span>
+				<span> 
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat placeat nemo quae, illum quam dignissimos consequuntur, accusamus quibusdam sunt praesentium, nesciunt corrupti asperiores pariatur molestias deserunt eius aliquid hic magnam. Tempore alias, hic amet, perspiciatis ea illo, soluta nesciunt magni deserunt cum adipisci? Corporis libero natus dolorum sequi, mollitia magnam ducimus eaque dolores pariatur praesentium expedita harum exercitationem ut id nam, quasi porro, tempora sunt quibusdam eos velit minus vel officiis! Dolores sunt veritatis qui magni minima vero amet adipisci? Culpa quisquam rem, error aperiam sunt reiciendis deserunt, aspernatur illo eaque dolorem veritatis? Quo velit magni eos ipsam quis error incidunt molestiae saepe corporis, pariatur eligendi minima. Itaque tempore asperiores error incidunt odit magni soluta sed expedita natus voluptatum beatae provident repellat molestiae temporibus quasi neque, magnam voluptatibus porro velit quisquam repellendus. Impedit modi, facere, officiis esse cum est cumque a veritatis labore blanditiis consectetur nesciunt porro iure. Obcaecati recusandae eum temporibus ad maiores vitae omnis pariatur, quasi quibusdam eius hic maxime nobis vero ea nihil harum cumque quam exercitationem amet necessitatibus. Distinctio, unde non. Mollitia, rerum quibusdam earum, ullam corporis aperiam, sint nulla quae voluptates quia tempora laudantium quas fugit adipisci veritatis qui exercitationem eaque labore necessitatibus
+				</span>
 
 			</div>
 		)
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
 import AboutMe from "../Components/AboutMe";
 
+const styles = {
+	backdrop: {
+		background: "green",
+		// height: "100vh",
+		// padding: "0",
+		// margin: "0",
+		  
+	},
+};
+
 class Home extends Component {
 
 
 	render(){
 
 		return(
-			<div>
+			<div style = {styles.backdrop}>
 
 				<AboutMe/>
 				{/* <Portfolio/> */}
