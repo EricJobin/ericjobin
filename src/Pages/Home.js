0f18b1e4a2e2809d8c43dbe4556743b1507dd284
unknown <ejobin@gmail.com>
2019-10-25 18:42:58 -0400

    have rudamentary nav bar working

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,7 +3,7 @@ import AboutMe from "../Components/AboutMe";
 
 const styles = {
 	backdrop: {
-		background: "green",
+		// background: "green",
 		// height: "100vh",
 		// padding: "0",
 		// margin: "0",
@@ -21,11 +21,24 @@ class Home extends Component {
 		// show: false,
 	};
 
+	tabclick = (tab)=>{
+		this.setState({ showaboutme: tab==="bio" ? true : false })
+		this.setState({ showportfolio: tab==="port" ? true : false })
+		this.setState({ showresume: tab==="cv" ? true : false })
+		this.setState({ showlinks: tab==="link" ? true : false })
+	}
+
 	render(){
 
 		return(
-			<div style = {styles.backdrop}>
-
+			// <div style = {styles.backdrop}>
+			<div>
+				<ul className="nav nav-tabs">
+					<li className="nav-item" onClick={() => this.tabclick('bio')}>Bio</li> | 
+					<li className="nav-item" onClick={() => this.tabclick('port')}>Portfolio</li> | 
+					<li className="nav-item" onClick={() => this.tabclick('cv')}>Resume</li> | 
+					<li className="nav-item" onClick={() => this.tabclick('link')}>Links</li> | 
+				</ul>
 				<AboutMe display={this.state.showaboutme} />
 				{/* <Portfolio/> */}
 				{/* <Resume/> */}
