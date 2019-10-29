de66f21cfd46a0ede9ec8315c54ea7bca5ac149d
unknown <ejobin@gmail.com>
2019-10-29 13:51:02 -0400

    Have hover effects working on portfolio cards

--- src/Components/PortfolioCard.js
+++ src/Components/PortfolioCard.js
@@ -3,42 +3,11 @@ import React, { Component } from "react";
 // import Card from 'react-bootstrap/Card';
 
 import {Button, Modal, Card, Col} from 'react-bootstrap';
+import "./cardstyle.css";
 
-const styles = {
-	card: {
-        width: "18rem",
-        // marginBottom: "5%",
-        // color: "red",
-		// float: "left",
-		// margin: "2% 5% 5% 5%",
-		// width: "100%",
-	},
-	col: {
-		// backgroundColor: "rgba(255,255,255,.6)",
-		// padding: "0% 2%",
-		// margin: "2%",
-		// minHeight: "54vh",
-		// position:"realative",
-	},
-	box: {
-		marginBottom: "5%",
-	}
-};
+// const styles = {
 
-// const PortfolioCard = (props) => {
-// 	return (
-// 		// console.log(props),
-//         <Col xs={12} sm={6} lg={4} style={styles.col}>
-//             <Card style={styles.card} id={props.id}>
-//                 <Card.Img variant="top" src={props.image} />
-//                 <Card.Body>
-//                     <h3>{props.title}</h3>
-//                 </Card.Body>
-//             </Card>
-//         </Col>
-
-// 	);
-// }
+// };
 
 class PortfolioCard extends Component {
     state = {
@@ -56,19 +25,12 @@ class PortfolioCard extends Component {
     render() {
     
         return (
-			console.log(this.props),
+			// console.log(this.props),
 			
-			<div style={styles.box}>
-				{/* <Button
-					// bsStyle="primary"
-					// bsSize="large"
-					onClick={this.open}
-				>
-					Launch demo modal
-				</Button> */}
+			<div className="box">
 
-				<Col xs={12} sm={6} lg={4} style={styles.col}>
-					<Card style={styles.card} id={this.props.id} onClick={this.open}>
+				<Col xs={12} sm={6} lg={4}>
+					<Card id={this.props.id} onClick={this.open} className="portCard">
 						<Card.Img variant="top" src={this.props.image} />
 						<Card.Body>
 							<h3>{this.props.title}</h3>
@@ -76,17 +38,14 @@ class PortfolioCard extends Component {
 					</Card>
 				</Col>
 
-
-
-
 				<Modal show={this.state.showModal} onHide={this.close}>
 					<Modal.Header closeButton>
-						<Modal.Title>Modal heading</Modal.Title>
+						<Modal.Title>{this.props.title}</Modal.Title>
 					</Modal.Header>
 					<Modal.Body>
-						<h4>Text in a modal</h4>
-						<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
-						{this.props.title}
+						<img alt={this.props.title} src={this.props.image} className="portimage"/>
+						
+						{this.props.blurb}
 					</Modal.Body>
 					<Modal.Footer>
 						<Button onClick={this.close}>Close</Button>
